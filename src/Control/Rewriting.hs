77cc7c3d6be4db67162739ed1bd8602456d072ef
Patrick Thomson <patrickt@github.com>
2020-03-18 19:46:21 -0400

    Format with Ormolu.

--- src/Control/Rewriting.hs
+++ src/Control/Rewriting.hs
@@ -1,43 +1,43 @@
-{-# LANGUAGE DeriveFunctor, DerivingVia, FlexibleContexts, FlexibleInstances, GADTs, LambdaCase, StandaloneDeriving, TupleSections,
+{-# LANGUAGE DerivingVia, FlexibleContexts, FlexibleInstances, GADTs, LambdaCase, StandaloneDeriving, TupleSections,
              TypeOperators #-}
+
+-- bottom-up tree transformations.
 
 -- | This module provides 'Rewrite', a monadic DSL that abstracts the
 -- details of rewriting a given datum into another type, supporting
 -- failure and retries. A set of /strategic combinators/, as described
 -- in Lämmel et al.'s /The Essence of Strategic Programming/, is
 -- provided to promote single-layer rewrites into top-down or
--- bottom-up tree transformations.
 module Control.Rewriting
   ( -- | Core types
-    Rewrite
-  , Rule
-  -- | Combinators
-  , target
-  , purely
-  , ensure
-  , try
-  , (>+>)
-  -- | Reexports from Control.Category
-  , (>>>)
-  , (<<<)
-  , (^>>)
-  -- | Predicate filtering
-  , refine
-  , only
-  -- | Useful rewrites
-  , mhead
-  , mjust
-  -- | Running rewrites
-  , rewrite
-  , recursively
-  -- | Strategic rewrite combinators
-  , applyAll
-  , applyAny
-  , topDownAll
-  , topDownAny
-  ) where
-
-import Prelude hiding (id, (.))
+    Rewrite,
+    Rule,
+    -- | Combinators
+    target,
+    purely,
+    ensure,
+    try,
+    (>+>),
+    -- | Reexports from Control.Category
+    (>>>),
+    (<<<),
+    (^>>),
+    -- | Predicate filtering
+    refine,
+    only,
+    -- | Useful rewrites
+    mhead,
+    mjust,
+    -- | Running rewrites
+    rewrite,
+    recursively,
+    -- | Strategic rewrite combinators
+    applyAll,
+    applyAny,
+    topDownAll,
+    topDownAny,
+  )
+where
 
 import           Control.Applicative
 import           Control.Arrow
@@ -51,6 +51,7 @@ import           Data.Maybe
 import           Data.Monoid
 import           Data.Profunctor (Choice, Profunctor, Strong)
 import qualified Data.Profunctor as P
+import           Prelude hiding (id, (.))
 
 -- | The fundamental type of rewriting rules. A @Rewrite t a@ maps
 -- values of type @t@ to values of type @a@, supporting failure and choice.
@@ -63,38 +64,46 @@ data Rewrite t a where
   -- We can copy NonDet to have fair conjunction or disjunction.
   Choice :: Rewrite t a -> Rewrite t a -> Rewrite t a
   Target :: Rewrite t t
-  Empty  :: Rewrite t a
-  Comp   :: Rewrite b c -> Rewrite a b -> Rewrite a c
+  Empty :: Rewrite t a
+  Comp :: Rewrite b c -> Rewrite a b -> Rewrite a c
   -- We could have implemented this by changing the semantics of how Then is interpreted, but that would make Then and Sequence inconsistent.
-  Match  :: (t -> Maybe u) -> Rewrite u a -> Rewrite t a
-  Then   :: Rewrite t b -> (b -> Rewrite t a) -> Rewrite t a
+  Match :: (t -> Maybe u) -> Rewrite u a -> Rewrite t a
+  Then :: Rewrite t b -> (b -> Rewrite t a) -> Rewrite t a
 
 -- | A 'Rule' is a 'Rewrite' with identical input and output types.
 type Rule t = Rewrite t t
 
 deriving via (WrappedMonad (Rewrite t)) instance Functor (Rewrite t)
-deriving via (WrappedArrow Rewrite t)   instance Applicative (Rewrite t)
-deriving via (WrappedArrow Rewrite t)   instance Alternative (Rewrite t)
-deriving via (SelectA (Rewrite t))      instance Selective (Rewrite t)
-deriving via (Alt (Rewrite t) t)        instance Semigroup (Rewrite t t)
-deriving via (Alt (Rewrite t) t)        instance Monoid (Rewrite t t)
-deriving via (P.WrappedArrow Rewrite)   instance Profunctor Rewrite
-deriving via (P.WrappedArrow Rewrite)   instance Strong Rewrite
-deriving via (P.WrappedArrow Rewrite)   instance Choice Rewrite
+
+deriving via (WrappedArrow Rewrite t) instance Applicative (Rewrite t)
+
+deriving via (WrappedArrow Rewrite t) instance Alternative (Rewrite t)
+
+deriving via (SelectA (Rewrite t)) instance Selective (Rewrite t)
+
+deriving via (Alt (Rewrite t) t) instance Semigroup (Rewrite t t)
+
+deriving via (Alt (Rewrite t) t) instance Monoid (Rewrite t t)
+
+deriving via (P.WrappedArrow Rewrite) instance Profunctor Rewrite
+
+deriving via (P.WrappedArrow Rewrite) instance Strong Rewrite
+
+deriving via (P.WrappedArrow Rewrite) instance Choice Rewrite
 
 instance Monad (Rewrite t) where
   (>>=) = Then
 
 -- | Rewrites are generally composed left-to-right with '>>>'.
 instance Category Rewrite where
-  id  = Target
+  id = Target
   (.) = Comp
 
 instance Arrow Rewrite where
   arr f = fmap f target
   first x = do
     d <- fmap snd target
-    P.dimap fst (, d) x
+    P.dimap fst (,d) x
 
 instance ArrowZero Rewrite where
   zeroArrow = Empty
@@ -137,6 +146,7 @@ only f = Match f Target
 -- | @a >+> b@ performs @a@ and @b@ in sequence, succeeding if one
 -- or both succeed.
 infixr 1 >+>
+
 (>+>) :: Rule t -> Rule t -> Rule t
 a >+> b = (a >>> try b) <|> b
 
@@ -161,27 +171,29 @@ mjust = only id
 rewrite :: (Alternative m, Monad m) => t -> Rewrite t a -> m a
 rewrite t = \case
   Choice a b -> rewrite t a <|> rewrite t b
-  Target     -> pure t
-  Match f m  -> foldMapA (`rewrite` m) (f t)
-  Comp g f   -> rewrite t f >>= (`rewrite` g)
-  Empty      -> empty
-  Then m f   -> rewrite t m >>= rewrite t . f
+  Target -> pure t
+  Match f m -> foldMapA (`rewrite` m) (f t)
+  Comp g f -> rewrite t f >>= (`rewrite` g)
+  Empty -> empty
+  Then m f -> rewrite t m >>= rewrite t . f
 
 -- | Run a 'Rewrite' over a 'Recursive' data structure, leaf-to-root. Unlike recursion with
 -- the Lämmel combinators, this allows you to use a 'Rewrite' rather than just a 'Rule',
 -- allowing for the returned type to change. If you pass in a 'Rule' here, consider whether
 -- the strategic combinators are actually what you want.
-recursively :: (Alternative m, Monad m, Corecursive t, Recursive t, Foldable (Base t))
-            => Rewrite t a
-            -> t
-            -> m a
+recursively ::
+  (Alternative m, Monad m, Corecursive t, Recursive t, Foldable (Base t)) =>
+  Rewrite t a ->
+  t ->
+  m a
 recursively m = para (paraRewrite m)
 
 type RAlgebra f t a = f (t, a) -> a
 
-paraRewrite :: (Alternative m, Monad m, Corecursive t, Foldable (Base t))
-            => Rewrite t a
-            -> RAlgebra (Base t) t (m a)
+paraRewrite ::
+  (Alternative m, Monad m, Corecursive t, Foldable (Base t)) =>
+  Rewrite t a ->
+  RAlgebra (Base t) t (m a)
 paraRewrite m t = rewrite (embedTerm t) m <|> foldMapA snd t
 
 -- * For more information about these combinators, see /The Essence of Strategic Programming/
@@ -189,26 +201,35 @@ paraRewrite m t = rewrite (embedTerm t) m <|> foldMapA snd t
 
 -- | Runs a 'Rule' one level deep in the provided 'Recursive' type, succeeding
 -- if all children succeed.
-applyAll :: (Corecursive t, Recursive t, Traversable (Base t))
-         => Rule t -> Rule t
+applyAll ::
+  (Corecursive t, Recursive t, Traversable (Base t)) =>
+  Rule t ->
+  Rule t
 applyAll m = id >>= \t -> Foldable.embed <$> traverse go (Foldable.project t)
-  where go a = pure a >>> m
+  where
+    go a = pure a >>> m
 
 -- | Runs a 'Rule' one level deep, succeeding if any children succeed.
-applyAny :: (Corecursive t, Recursive t, Traversable (Base t))
-         => Rule t -> Rule t
+applyAny ::
+  (Corecursive t, Recursive t, Traversable (Base t)) =>
+  Rule t ->
+  Rule t
 applyAny m = applyAll (m <|> id)
 
 -- | Runs a 'Rule' top-down over a 'Recursive' type, succeeding if
 -- all subsequent rewrites succeed.
-topDownAll :: (Corecursive t, Recursive t, Traversable (Base t))
-           => Rule t -> Rule t
+topDownAll ::
+  (Corecursive t, Recursive t, Traversable (Base t)) =>
+  Rule t ->
+  Rule t
 topDownAll r = let go = r >>> applyAll go in go
 
 -- | Runs a 'Rule' top-down over a 'Recursive' type, succeeding if
 -- any rewrites succeed.
-topDownAny :: (Corecursive t, Recursive t, Traversable (Base t))
-           => Rule t -> Rule t
+topDownAny ::
+  (Corecursive t, Recursive t, Traversable (Base t)) =>
+  Rule t ->
+  Rule t
 topDownAny r = let go = r >+> applyAny go in go
 
 embedTerm :: Corecursive t => Base t (t, a) -> t
